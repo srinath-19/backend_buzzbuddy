26bd4651f6a5568df8b72a5e598082195827c47f
srinath-19 <srinath.m1902@gmail.com>
2025-04-21 22:23:15 -0600

    5

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,12 @@ const app: Application = express();
 
 // * Middleware
 app.use(cors(
-//   {
-//   origin: ["https://demo123-xvrs.vercel.app"],  // Allow only your frontend domain
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true, // if you're using cookies or auth headers
-// }
+  {
+  origin: ["https://demo123-xvrs.vercel.app"],  // Allow only your frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // if you're using cookies or auth headers
+}
 ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
